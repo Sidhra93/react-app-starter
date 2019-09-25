52dea9f1b81a7ac49504b297d9f9cce8d40c653d
Sidhra Cader <scader@mel0bhmac559.ap.corp.jwt.com>
2019-09-25 12:55:12 +1000

    add state

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import React from 'react'
 import HelloWorld from './components/HelloWorld'
 
 class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
     render() {
         return (
             <div>
